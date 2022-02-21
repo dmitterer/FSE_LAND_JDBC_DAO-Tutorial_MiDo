bedb8d4ea1aa054ad1d2e2fc7b5815334eb08318
Dominik <dmitterer@tsn.at>
2022-02-21 22:57:23 +0100

    Erstellen von "MySqlCourseRepository.java" und "DatabaseException.java ". (siehe Worddokument Unterpunkt: 5.9.1 und 5.9.2) Änderungenen an "Cli.java" und "Main.java" siehe Worddokument Unterpunkt: 5.9.3 und 5.9.4)

---
+++ src/main/java/dataaccess/DatabaseException.java
@@ -0,0 +1,8 @@
+package dataaccess;
+
+public class DatabaseException extends RuntimeException {
+    public DatabaseException(String message) {
+
+        super(message);
+    }
+}
--- src/main/java/ui/Cli.java
+++ src/main/java/ui/Cli.java
@@ -1,16 +1,22 @@
 package ui;
 
+import dataaccess.DatabaseException;
+import dataaccess.MyCourseRepository;
+import dataaccess.MySqlCourseRepository;
+import domain.Course;
+
+import java.util.List;
 import java.util.Scanner;
 
 public class Cli {
 
     Scanner scan;
+    MySqlCourseRepository repo;
 
-    public Cli()
+    public Cli(MySqlCourseRepository repo)
     {
-
-
         this.scan = new Scanner(System.in);
+        this.repo = repo;
     }
 
     public void start()
@@ -30,7 +36,7 @@ public class Cli {
                 break;
 
             case "2":
-                System.out.println("Alle Kurse anzeigen");
+                showAllCourses();
                 break;
 
             case "x":
@@ -42,6 +48,39 @@ public class Cli {
         }
     }scan.close();
 
+    }
+
+    private void showAllCourses() {
+        List<Course> list = null;
+
+
+        try {
+
+
+        list = repo.getAll();
+
+        if (list.size()>0)
+        {
+            for (Course course : list)
+            {
+                System.out.println(course);
+
+            }
+        }else
+        {
+            System.out.println("Kursliste Leer!");
+        }
+        }catch (DatabaseException databaseException)
+        {
+
+            System.out.println("Datenbankfehler bei Anzeige aller Kurse " + databaseException.getMessage());
+
+        }catch (Exception exception)
+        {
+
+            System.out.println("Unbekannter Fehler bei Anzeige aller Kurse: " + exception.getMessage());
+
+        }
     }
 
     private void showMenue()
