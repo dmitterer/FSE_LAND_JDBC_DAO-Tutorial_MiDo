8eb69b1111fd5114fa46ea63aa95615bec5d0785
Dominik <dmitterer@tsn.at>
2022-02-22 21:47:29 +0100

    Ausbesserung der Methode getAll:
    Return-Statement war falsch positioniert. (siehe Worddokument Unterpunkt: 5.9.6)

--- src/main/java/dataaccess/MySqlCourseRepository.java
+++ src/main/java/dataaccess/MySqlCourseRepository.java
@@ -48,15 +48,15 @@ public class MySqlCourseRepository implements MyCourseRepository {
                                 CourseType.valueOf(resultSet.getString("coursetype"))
                         )
                 );
-                return courseList;
 
-            }
+
+            }return courseList;
 
         } catch (SQLException e) {
             throw new DatabaseException("Database error occoured!");
         }
 
-        return null;
+
     }
 
     @Override
